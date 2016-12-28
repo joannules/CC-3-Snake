8533990c9f16a81563384c5ad64eee256dcb126d
joannules <joanfelip_nls@hotmail.com>
2016-12-28 23:24:49 +0100

    Versio coxina

--- sketch.js
+++ sketch.js
@@ -1,33 +1,60 @@
 var s;
 var scl = 20;
 var food;
+var foods = [];
+var img = [];
 
 function setup() {
-    canvas = createCanvas(floor(650/scl)*scl, floor(450/scl)*scl);
+    canvas = createCanvas(floor(650 / scl) * scl, floor(450 / scl) * scl);
     canvas.parent('sketch_holder');
+    loadTiosSexys();
     s = new Snake();
     frameRate(10);
-    pickLocation();
+    if (!s.avaricia) {
+        food = pickLocation();
+    } else {
+        for (i = 0; i < 15; i++) {
+            foods[i] = pickLocation();
+        }
+    }
 }
 
 function draw() {
-    background(60);
-    s.death();
-    s.update();
-    s.show();
-    if (s.eat(food)) {
-        pickLocation();
+    if (s.alive) {
+        background(60);
+        if (s.desconcetracio) {
+            image(img[s.count], 0, 0);
+        }
+        background(60,50);
+        s.death();
+        s.update();
+        s.show();
+        if (!s.avaricia) {
+            if (s.eat(food)) {
+                food = pickLocation();
+            }
+            fill(255, 0, 60);
+            rect(food.x, food.y, scl, scl);
+        } else {
+            for (i = 0; i < 15; i++) {
+                if (s.eat(foods[i])) {
+                    foods[i] = pickLocation();
+                }
+                fill(255, 0, 60);
+                rect(foods[i].x, foods[i].y, scl, scl);
+            }
+        }
+    } else {
+        document.getElementById("header2").innerHTML = 'paverse matau';
     }
-
-    fill(255, 0, 60);
-    rect(food.x, food.y, scl, scl);
-
+    document.getElementById("score").innerHTML = "Score:" + s.total*10;
 }
 
 function pickLocation() {
     var cols = floor(width / scl);
     var rows = floor(height / scl);
-    food = createVector(scl*floor(random(cols)), scl*floor(random(rows)));
+    posi = createVector(scl * floor(random(cols)), scl * floor(random(rows)));
+    return posi;
 }
 
 
@@ -41,4 +68,29 @@ function keyPressed() {
     } else if (keyCode === RIGHT_ARROW && s.xspeed != -1) {
         s.dir(1, 0);
     }
+}
+
+function loadTiosSexys() {
+    img[0] = loadImage("assets/tia1.jpg");
+    img[1] = loadImage("assets/tia2.jpg");
+    img[2] = loadImage("assets/tia3.jpg");
+    img[3] = loadImage("assets/tia4.jpg");
+    img[4] = loadImage("assets/tia5.jpg");
+    img[5] = loadImage("assets/tia6.jpg");
+    img[6] = loadImage("assets/tia7.jpg");
+    img[7] = loadImage("assets/tia8.jpg");
+    img[8] = loadImage("assets/tia9.jpg");
+    img[9] = loadImage("assets/tia10.jpg");
+    img[10] = loadImage("assets/tio1.jpg");
+    img[11] = loadImage("assets/tio2.jpg");
+    img[12] = loadImage("assets/tio3.jpg");
+    img[13] = loadImage("assets/tio4.jpg");
+    img[14] = loadImage("assets/tio5.jpg");
+    img[15] = loadImage("assets/tio6.jpg");
+    img[16] = loadImage("assets/tio7.jpg");
+    img[17] = loadImage("assets/tio8.jpg");
+    img[18] = loadImage("assets/tio9.jpg");
+    img[19] = loadImage("assets/tio10.jpg");
+    img[20] = loadImage("assets/tio10.jpg");
+    img[21] = loadImage("assets/tio10.jpg");
 }
--- snake.js
+++ snake.js
@@ -5,8 +5,31 @@ function Snake() {
     this.yspeed = 0;
     this.total = 0;
     this.tail = [];
+    this.alive = true;
+    this.avaricia = false;
+    this.alpha = 255;
+    this.desconcetracio = false;
+    this.count = 0;
 
     this.update = function() {
+
+        if (this.total < 10) {
+            this.mode1();
+        } else if (this.total < 20) {
+            this.mode2();
+        } else if (this.total < 30) {
+            this.mode3();
+        } else if (this.total < 40) {
+            this.mode4();
+        } else if (this.total < 50) {
+            this.mode5();
+        } else if (this.total < 60) {
+            this.mode6();
+        } else if (this.total < 70) {
+            this.mode7();
+        } else {
+            this.mode1();
+        }
 
         if (this.total === this.tail.length) {
             this.tail[this.total] = createVector(this.x, this.y);
@@ -27,7 +50,7 @@ function Snake() {
     }
 
     this.show = function() {
-        fill(255);
+        fill(255, this.alpha);
         noStroke();
         for (var i = 0; i < this.tail.length; i++) {
             rect(this.tail[i].x, this.tail[i].y, scl, scl);
@@ -45,6 +68,14 @@ function Snake() {
         var d = dist(this.x, this.y, pos.x, pos.y);
         if (d < 1) {
             this.total++;
+            if (this.invisibilitat) {
+                this.alpha = this.alpha - 25;
+            } else {
+                this.alpha = 255;
+            }
+            if (this.desconcetracio) {
+                this.count = this.count + 1;
+            }
             return true;
         } else {
             return false;
@@ -58,7 +89,59 @@ function Snake() {
             if (d < 1) {
                 this.total = 0;
                 this.tail = [];
+                this.alive = false;
             }
         }
     }
+
+    this.mode1 = function() {
+            document.getElementById("header2").innerHTML = 'Nivell 1 : Normal';
+        }
+        // mode 2 = atac epileptic
+    this.mode2 = function() {
+            document.getElementById("header2").innerHTML = 'Nivell 2 : L\'atac depilepsia';
+            background(random(255), random(255), random(255));
+        }
+        // mode3 = La desconcentració
+    this.mode3 = function() {
+            document.getElementById("header2").innerHTML = 'Nivell 3 : La desconcetració';
+            this.desconcetracio = true;
+        }
+        // mode4 = L'avaricia
+    this.mode4 = function() {
+            document.getElementById("header2").innerHTML = 'Nivell 4 : La gula';
+            background(60);
+            this.desconcetracio = false;
+            if (!this.avaricia) {
+                for (i = 0; i < 15; i++) {
+                    foods[i] = pickLocation();
+                }
+            }
+            this.avaricia = true;
+        }
+        // mode5 doble velocitat
+    this.mode5 = function() {
+            document.getElementById("header2").innerHTML = 'Nivell 5 : Exces de velocitat';
+            this.avaricia = false;
+            frameRate(20);
+        }
+        // mode6 invisibilitat
+    this.mode6 = function() {
+        document.getElementById("header2").innerHTML = 'Nivell 6 : Invisibilitat';
+        frameRate(10);
+        this.invisibilitat = true;
+    }
+     // desconcetracio2
+    this.mode7 = function() {
+      this.invisibilitat = false;
+      document.getElementById("header2").innerHTML = 'Nivell 3 : La desconcetració';
+      this.desconcetracio = true;
+    }
+
+    // la gran fartada
+    // this.mode8 = function() {
+    //     document.getElementById("header2").innerHTML = 'La gran fartada x3';
+    //     this.invisibilitat = false;
+    //     this.fartada = true;
+    // }
 }
